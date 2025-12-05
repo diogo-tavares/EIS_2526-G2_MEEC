a2b8a8c1c09ad1195e04a9d38846517c9e097c0a
diogo-tavares <78868918+diogo-tavares@users.noreply.github.com>
2025-12-05 12:47:25 +0000

    Atualizações segundo o que o professor mencionou
    - tipos de coleção substituidos por tags
    - paginas de editar e adicionar coleção alteradas para estar de acordo com a utilização de tags

--- Teamwork/public_html/js/add_edit_colection.js
+++ Teamwork/public_html/js/add_edit_colection.js
@@ -8,11 +8,20 @@ document.addEventListener("DOMContentLoaded", function () {
 
         const name = document.getElementById("collection-name").value.trim();
         const date = document.getElementById("collection-date").value.trim();
-        const type = document.getElementById("collection-type").value.trim();
         const description = document.getElementById("collection-description").value.trim();
-
+        
+        // Lê os 5 campos
+        const t1 = document.getElementById("tag-1").value.trim();
+        const t2 = document.getElementById("tag-2").value.trim();
+        const t3 = document.getElementById("tag-3").value.trim();
+        const t4 = document.getElementById("tag-4").value.trim();
+        const t5 = document.getElementById("tag-5").value.trim();
+        
+        // Junta tudo e remove vazios
+        const tags = [t1, t2, t3, t4, t5].filter(tag => tag !== "");
+        
         // Verifica se todos os campos estão preenchidos
-        if (!name || !date || !type || !description) {
+        if (!name || !date || !description || tags.length === 0) {
             alert("Por favor, preencha todos os campos antes de confirmar!");
             return;
         }
